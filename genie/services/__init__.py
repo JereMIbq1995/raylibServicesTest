45e7edfd6fe47f78e082c5e3a7e2dff5ee514861
jeremi1995 <jeremibq@gmail.com>
2021-11-15 12:26:34 -0700

    modified constants handling for keyboard and mouse services

--- genie/services/__init__.py
+++ genie/services/__init__.py
@@ -3,12 +3,16 @@ from .pygame_services.PygameScreenService import PygameScreenService
 from .pygame_services.PygameKeyboardService import PygameKeyboardService
 from .pygame_services.PygameMouseService import PygameMouseService
 from .pygame_services.PygamePhysicsService import PygamePhysicsService
+from .pygame_services.constants import keys as pygame_keys
+from .pygame_services.constants import mouse as pygame_mouse
 
 from .raylib_services.RaylibAudioService import RaylibAudioService
 from .raylib_services.RaylibScreenService import RaylibScreenService
 from .raylib_services.RaylibKeyboardService import RaylibKeyboardService
 from .raylib_services.RaylibMouseService import RaylibMouseService
 from .raylib_services.RaylibPhysicsService import RaylibPhysicsService
+from .raylib_services.constants import keys as raylib_keys
+from .raylib_services.constants import mouse as raylib_mouse
 
 __all__ = [
     'PygameAudioService',
@@ -16,10 +20,14 @@ __all__ = [
     'PygameKeyboardService',
     'PygameMouseService',
     'PygamePhysicsService',
+    'pygame_keys',
+    'pygame_mouse',
 
     'RaylibAudioService',
     'RaylibScreenService',
     'RaylibKeyboardService',
     'RaylibMouseService',
-    'RaylibPhysicsService'
+    'RaylibPhysicsService',
+    'raylib_keys',
+    'raylib_mouse'
 ]
