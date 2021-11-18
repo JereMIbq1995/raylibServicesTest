d0e91f65398d78bc46bb02560aca1fe818382d56
Jeremy Duong <jeremibq@gmail.com>
2021-11-18 15:08:37 -0700

    added constants for colors

---
+++ genie/services/constants/colors.py
@@ -0,0 +1,19 @@
+"""
+    Here are a few basic colors defined in RGB format.
+    
+    Also note that there can be a 4th entry that specifies
+    opacity of the color. For example, a see-through BLACK would be:
+    (0, 0, 0, 100)
+
+    Opacity goes from 0 to 255, with 0 being completely vanished and
+        255 being completely not see-through-able
+"""
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
