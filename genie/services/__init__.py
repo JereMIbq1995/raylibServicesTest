f4b4e5bf86ceb31a72412afd63d10867249bc7f4
Jeremy Duong <jeremibq@gmail.com>
2021-11-18 14:22:01 -0700

    simplified ui constants import for the user

--- genie/services/__init__.py
+++ genie/services/__init__.py
@@ -3,16 +3,15 @@ from .pygame_services.PygameScreenService import PygameScreenService
 from .pygame_services.PygameKeyboardService import PygameKeyboardService
 from .pygame_services.PygameMouseService import PygameMouseService
 from .pygame_services.PygamePhysicsService import PygamePhysicsService
-from .pygame_services.constants import keys as pygame_keys
-from .pygame_services.constants import mouse as pygame_mouse
 
 from .raylib_services.RaylibAudioService import RaylibAudioService
 from .raylib_services.RaylibScreenService import RaylibScreenService
 from .raylib_services.RaylibKeyboardService import RaylibKeyboardService
 from .raylib_services.RaylibMouseService import RaylibMouseService
 from .raylib_services.RaylibPhysicsService import RaylibPhysicsService
-from .raylib_services.constants import keys as raylib_keys
-from .raylib_services.constants import mouse as raylib_mouse
+
+from .constants import keys
+from .constants import mouse
 
 __all__ = [
     'PygameAudioService',
@@ -20,14 +19,13 @@ __all__ = [
     'PygameKeyboardService',
     'PygameMouseService',
     'PygamePhysicsService',
-    'pygame_keys',
-    'pygame_mouse',
 
     'RaylibAudioService',
     'RaylibScreenService',
     'RaylibKeyboardService',
     'RaylibMouseService',
     'RaylibPhysicsService',
-    'raylib_keys',
-    'raylib_mouse'
+    
+    'keys',
+    'mouse'
 ]
